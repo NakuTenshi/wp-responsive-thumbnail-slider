f0650c98db110bd111eecac6c367cbb733157c24
nik00726 <nik00726@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-22 15:02:34 +0000

    small bugfix
    git-svn-id: https://plugins.svn.wordpress.org/wp-responsive-thumbnail-slider/trunk@776785 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-responsive-images-thumbnail-slider.php
+++ wp-responsive-images-thumbnail-slider.php
@@ -832,6 +832,24 @@
                 $title=trim(addslashes($_POST['imagetitle']));
                 $imageurl=trim($_POST['imageurl']);
                 $createdOn=date('Y-m-d h:i:s');
+                if(function_exists('date_i18n')){
+                    
+                    $createdOn=date_i18n('Y-m-d'.' '.get_option('time_format') ,false,false);
+                    if(get_option('time_format')=='H:i')
+                        $createdOn=date('Y-m-d H:i:s',strtotime($createdOn));
+                    else   
+                        $createdOn=date('Y-m-d h:i:s',strtotime($createdOn));
+                        
+                }
+                if(function_exists('date_i18n')){
+                    
+                    $createdOn=date_i18n('Y-m-d'.' '.get_option('time_format') ,false,false);
+                    if(get_option('time_format')=='H:i')
+                        $createdOn=date('Y-m-d H:i:s',strtotime($createdOn));
+                    else   
+                        $createdOn=date('Y-m-d h:i:s',strtotime($createdOn));
+                        
+                }
                 
                 if ($_FILES["image_name"]["error"] > 0)
                 {
